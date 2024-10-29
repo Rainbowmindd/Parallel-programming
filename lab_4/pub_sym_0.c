1d0ceb88ac2b97ce29ccafc9294949673bd8b655
MartynaK <martyna.kindrat@gmail.com>
2024-10-29 18:35:00 +0100

    S0 i S1 for lab4

--- lab_4/pub_sym_0.c
+++ lab_4/pub_sym_0.c
@@ -5,15 +5,12 @@
 #include <unistd.h>
 
 #define ILE_MUSZE_WYPIC 3
-//pub_sym_1 -> bez zabezpieczen, sledzenie liczby kufli, wypisanie iloscina zakonczeniu symulacji
+//pub_sym_0 -> bez zabezpieczen, sledzenie liczby kufli, wypisanie iloscina zakonczeniu symulacji
 //klient to watek
 void * watek_klient (void * arg);
 
 int l_kf; //liczba kufli
 int uzywany_kufel=0; 
-
-//deklaracja mutexu
-pthread_mutex_t mutex;
 
 int main( void ){
 
@@ -26,8 +23,6 @@ int main( void ){
 
   printf("\nLiczba kufli: "); scanf("%d", &l_kf);
 
-  //inicjalizacja mutexu
-  pthread_mutex_init(&mutex,NULL);
   //printf("\nLiczba kranow: "); scanf("%d", &l_kr);
   l_kr = 1000000000; // wystarczajaco duzo, zeby nie bylo rywalizacji 
 
@@ -48,8 +43,6 @@ int main( void ){
   printf("\nZamykamy pub!\n"); 
   printf("Ilosc uzytych kufli: %d\n",uzywany_kufel);
 
-  pthread_mutex_destroy(&mutex);
-
 
 }
 
@@ -68,14 +61,10 @@ void * watek_klient (void * arg_wsk){
   for(i=0; i<ile_musze_wypic; i++){
 
     printf("\nKlient %d, wybieram kufel\n", moj_id); 
-
-    //zabezpieczamy dostep do zmiennej
-    pthread_mutex_lock(&mutex);
     
     if(uzywany_kufel<l_kf){
         uzywany_kufel++;
         printf("\nAktualnie zuzyto %d kufli\n",uzywany_kufel);
-        pthread_mutex_unlock(&mutex); //odblokowanie dostepu do zmiennej
 
         j=0;//nr kranu
         printf("\nKlient %d, nalewam z kranu %d\n", moj_id, j); 
